e79c699c4b931cdc244df513c70b35b4e8456794
Kate Lytvynets <kalytv@microsoft.com>
2017-10-26 15:20:35 -0700

    Adjust search button and correct functionality

--- src/visual.ts
+++ src/visual.ts
@@ -23,25 +23,6 @@
  *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  *  THE SOFTWARE.
  */
-module powerbi.extensibility.visual {
-  export function logExceptions(): MethodDecorator {
-    return function (target: Object, propertyKey: string, descriptor: TypedPropertyDescriptor<Function>)
-      : TypedPropertyDescriptor<Function> {
-
-      return {
-        value: function () {
-          try {
-            return descriptor.value.apply(this, arguments);
-          } catch (e) {
-            console.error(e);
-            throw e;
-          }
-        }
-      }
-    }
-  }
-}
-
 
 import DataViewObjects = powerbi.extensibility.utils.dataview.DataViewObjects;
 
@@ -58,10 +39,9 @@ module powerbi.extensibility.visual {
         private dataViewObjectParser: powerbi.extensibility.utils.dataview.DataViewObjectsParser;
 
         constructor(options: VisualConstructorOptions) {
-          debugger;
             this.target = options.element;
             this.target.innerHTML = `<div class="text-filter-search">
-                                        <input aria-label="Enter your search" type="search" placeholder="Search" name="search-field">
+                                        <input aria-label="Enter your search" type="text" placeholder="Search" name="search-field">
                                         <button class="c-glyph search-button" name="search-button">
                                           <span class="x-screen-reader">Search</span>
                                         </button>
@@ -69,11 +49,6 @@ module powerbi.extensibility.visual {
                                           <span class="x-screen-reader">Clear</span>
                                         </button>
                                     </div>`;
-
-            //this.searchButton = this.target.childNodes[0].childNodes[3] as HTMLButtonElement;
-            //this.searchButton.addEventListener("click", () => this.performSearch(this.searchBox.value));
-            //this.clearButton = this.target.childNodes[1].childNodes[5] as HTMLButtonElement;
-            //this.clearButton.addEventListener("click", () => this.performSearch(''));
 
             this.searchBox = this.target.childNodes[0].childNodes[1] as HTMLInputElement;
             this.searchBox.addEventListener("change", (e) => this.performSearch(this.searchBox.value));
@@ -87,21 +62,19 @@ module powerbi.extensibility.visual {
         }
 
         performSearch(text) {
-          debugger;
             if (this.column) {
                 const target = {
-                    table: this.column.queryName.substr(0, this.column.queryName.indexOf('.')),
-                    column: this.column.displayName
+                  table: this.column.queryName.substr(0, this.column.queryName.indexOf('.')),
+                  column: this.column.displayName
                 };
 
-
                 const filter = new models.AdvancedFilter(
-                    target,
-                    "And",
-                    {
-                        operator: "Contains",
-                        value: text
-                    }
+                  target,
+                  "And",
+                  {
+                      operator: "Contains",
+                      value: text
+                  }
                 );
 
                 this.host.applyJsonFilter(filter, "general", "filter");
@@ -119,7 +92,6 @@ module powerbi.extensibility.visual {
         }
 
         public update(options: VisualUpdateOptions) {
-          debugger;
             const metadata = options.dataViews && options.dataViews[0] && options.dataViews[0].metadata;
             const newColumn = metadata && metadata.columns && metadata.columns[0];
             const properties = DataViewObjects.getObject(options.dataViews[0].metadata.objects, "general"); 
@@ -129,7 +101,6 @@ module powerbi.extensibility.visual {
             };
 
             this.column = newColumn;
-            console.log("Text:" + properties["searchText"] )
             this.searchBox.value = (properties["searchText"]) ? properties["searchText"].toString() : '';
         }
     }
